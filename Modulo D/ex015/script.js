f54f45d6256c2a4e069933a7d20fd935221b96a6
Jonathas Tavares <jonathasl.tavares@gmail.com>
2022-09-13 16:16:04 -0300

    Corrigindo Modulo D

--- Modulo D/ex015/script.js
+++ Modulo D/ex015/script.js
@@ -1,21 +1,52 @@
-var bttnver = window.document.getElementById('botao')
-bttnver.addEventListener('click', verificar)
-
-    
 function verificar (){
     var data = new Date()
     var ano = data.getFullYear()
     var fano = document.getElementById('txtano')
+    var sexo = document.getElementById('')
     var res = document.getElementById('res')
 
     if (fano.value.length === 0 || fano.value > ano || fano.value == null){
     window.alert('ERRO! Esta pessoa certamente não existe')
-}   
-    else  {
+    }
+    else {
+        var fsex = document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
-        res.style.textAlign='center'
-        res.innerHTML = `Em ${ano} essa pessoa tem/faz ${idade} anos.`
-}
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        var sexo = ''
+        if(fsex[0].checked){
+            sexo = 'masculino'
+            if(idade>=0 && idade<=10){
+                img.setAttribute('src', 'image/criancaMasc.jpg')
+            }
+            else if(idade>10 && idade<21){
+                img.setAttribute('src', 'image/jovemMasc.jpg')
+            }
+            else if(idade>21 && idade<50){
+                img.setAttribute('src', 'image/adultoMasc.jpg')
+            }
+            else if(idade>50){
+                img.setAttribute('src', 'image/idosoMasc.jpg')
+            }
+        }else if(fsex[1].checked){
+            sexo = 'feminino'
+            if(idade>=0 && idade<=10){
+                img.setAttribute('src', 'image/criancaFem.jpg')
+            }
+            else if(idade>10 && idade<21){
+                img.setAttribute('src', 'image/jovemFem.jpg')
+            }
+            else if(idade>21 && idade<50){
+                img.setAttribute('src', 'image/adultoFem.jpg')
+            }
+            else if(idade>50){
+                img.setAttribute('src', 'image/idosoFem.jpg')
+            }
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos uma pessoa do sexo ${sexo} com ${idade} anos`
+        res.appendChild(img)
+    }
 }
    
 
